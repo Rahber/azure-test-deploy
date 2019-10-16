6b4a008e79bf04087eb8f51fc7d72d3c126ace2a
Rahber Ashraf <rahber50@gmail.com>
2019-10-16 14:37:35 +0500

    Update linkedin.php
    Update

--- linkedin.php
+++ linkedin.php
@@ -3,17 +3,15 @@ $response_type = "code";
 $client_id = "81j99zm1ygvajl";
 $redirect_uri = "https://app.rahberashraf.com/linkedin.php";
 $client_secret = "aFdjSw3mKv8rdMYN";
-$scope = "";
+$scope = "scope=r_liteprofile%20r_emailaddress%20w_member_social";
 $code = "";
 
-if($step==1){
+if($_GET['code']){
 $code = $_GET['code'];
 }else if ($step==2){
 	
 	
-	$string = POST /oauth/v2/accessToken HTTP/1.1
-Host: www.linkedin.com
-Content-Type: application/x-www-form-urlencoded
+
 
 $string = "grant_type=authorization_code&code=".$code."&redirect_uri=".$redirect_uri."&client_id=".$client_id."&client_secret=".$client_secret;
 }
@@ -26,3 +24,5 @@ $string = "grant_type=authorization_code&code=".$code."&redirect_uri=".$redirect
 
 
 <a href ="" ><img src="https://content.linkedin.com/content/dam/developer/global/en_US/site/img/signin-button.png" /></a>
+
+The Access Code is <input type="text><?php echo $code; ?></input>
