153f737e1d6d277120e5b91f4720b1762c1f335d
Rahber Ashraf <rahber50@gmail.com>
2019-10-16 14:57:18 +0500

    Update linkedin.php
    Update

--- linkedin.php
+++ linkedin.php
@@ -3,7 +3,7 @@ $response_type = "code";
 $client_id = "81j99zm1ygvajl";
 $redirect_uri = "https://app.rahberashraf.com/linkedin.php";
 $client_secret = "aFdjSw3mKv8rdMYN";
-$scope = "scope=r_liteprofile%20r_emailaddress%20w_member_social";
+$scope = "r_liteprofile%20r_emailaddress%20w_member_social";
 $code = "";
 
 
@@ -11,9 +11,15 @@ $firstString ="https://www.linkedin.com/oauth/v2/authorization?response_type=".$
 if($_GET['code']){
 $code = $_GET['code'];
 }else if ($step==2){
+	$ch = curl_init();
+	$request_headers = array(
+                    "X-Mashape-Key:" . $subscription_key,
+                    "X-Mashape-Host:" . $host
+                );	
 	
-	
-
+	curl_setopt($ch, CURLOPT_URL, "https://www.linkedin.com/oauth/v2/accessToken");
+               // curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+                $output = json_encode(curl_exec($ch));
 
 $string = "grant_type=authorization_code&code=".$code."&redirect_uri=".$redirect_uri."&client_id=".$client_id."&client_secret=".$client_secret;
 }
