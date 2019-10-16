d27dfb71097e6cda53fc3babcf5bebcb64bfecac
Rahber Ashraf <rahber50@gmail.com>
2019-10-16 17:07:26 +0500

    Update linkedin.php
    added profile

--- linkedin.php
+++ linkedin.php
@@ -13,8 +13,7 @@ $firstString ="https://www.linkedin.com/oauth/v2/authorization?response_type=".$
 if($_GET['code']){
 $code = $_GET['code'];
 }
-if($_POST){
-	
+if($_POST['frmAction']=="access"){
 	$ch = curl_init();
 	$request_headers = array(
                     "POST /oauth/v2/accessToken HTTP/1.1",
@@ -25,8 +24,8 @@ if($_POST){
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $secondString );
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_URL, "https://www.linkedin.com/oauth/v2/accessToken");
-	echo $result = (curl_exec($ch));
-	 $resultt = json_decode($result);
+	$result = (curl_exec($ch));
+	$resultt = json_decode($result);
 	
 	
 	if($resultt->access_token){
@@ -36,9 +35,36 @@ if($_POST){
 	}else{
 		$oo = "There was an error";
 	}
-	
+	curl_close($ch);
 
+}else if($_POST['frmAction']=="profile"){
+	$access_token = $_POST['access_token'];
+$ch = curl_init();
+	$request_headers = array(
+                    "GET /v2/me HTTP/1.1",
+                    "Host: api.linkedin.com",
+					"Connection: Keep-Alive",
+					"Authorization: Bearer ".$access_token
+                );	
+	//$secondString = "client_id=".$client_id."&grant_type=authorization_code&code=".$code."&redirect_uri=".$redirect_uri."&client_secret=".$client_secret;
+	//curl_setopt($ch, CURLOPT_POSTFIELDS, $secondString );
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, "https://api.linkedin.com/v2/me");
+	echo $result = (curl_exec($ch));
+	$resultt = json_decode($result);
+	
+	
+	if($resultt->access_token){
+		$profile = $resultt->access_token;
+	}else if($resultt->error){
+		$profile = $resultt->error;
+	}else{
+		$profile = "There was an error";
+	}
+	curl_close($ch);	
 }
+		
+		
 
 
 
@@ -54,10 +80,16 @@ if($_POST){
 <br />
 <br />
 <form action="<?php echo $current_uri?>" method="post">
-<input type="hidden" name="hidden">
-<input type="submit" value="Submit form">
+<input type="hidden" name="frmAction" value="access">
+The Authorization Code is <input type="text" value="<?php echo $code; ?>" />
+<input type="submit" value="Fetch Access Code">
 </form>
 
-<br />
-The Authorization Code is <input type="text" value="<?php echo $code; ?>" /><br />
-The Access Code is <input type="text" value="<?php echo $oo; ?>" />
+<br /><br />
+
+<form action="<?php echo $current_uri?>" method="post">
+<input type="hidden" name="frmAction" value="profile">
+The Access Code is <input type="text" name ="access_token" value="<?php echo $oo; ?>" />
+<input type="submit" value="Fetch Profile">
+</form><br />
+The Profile is <input type="text" value="<?php echo $profile; ?>" /><br />
